a6c349da03d96a2f9d6bae123ffdddf0de6c313c
chris <chrisjos007@gmail.com>
2020-11-04 17:31:21 +0530

    formats the code better

--- population_estimates.py
+++ population_estimates.py
@@ -7,71 +7,97 @@ plt.autoscale(enable=True, axis='both', tight=None)
 
 
 def india_plot(csv_read):
+    # function to plot the year vs poplation for India
+
     x = []
     y = []
+
     for line in csv_read:
         if(line[0] == "India"):
             x.append(int(line[2]))
             y.append(float(line[3]))
+
+    # plotting graph
     plt.bar(x, y)
     plt.xlabel("Year")
     plt.ylabel("Population")
-    plt.title("India - Population vs Year")
+    plt.title("India -Year vs Population")
     plt.show()
 
 
-def asean_plot(csv_read, ASEAN):
+def asean_plot(csv_read, asean):
+    # to plot the bar graph of asean nations
+
     x = []
     y = []
+
     for line in csv_read:
-        if((line[0] in ASEAN) and (line[2] == "2014")):
+        if((line[0] in asean) and (line[2] == "2014")):
             x.append(line[0])
             y.append(float(line[3]))
+
     plt.figure(figsize=(8, 4))
     plt.bar(x, y, width=0.4)
     plt.xlabel("Country")
     plt.ylabel("Population")
-    plt.title("ASEAN - Countries vs Population")
+    plt.title("ASEAN - Country vs Population")
     plt.show()
-    return ASEAN
 
 
-def saarc_plot(csv_read, SAARC):
+def saarc_plot(csv_read, saarc):
+    # to plot the group plot of ASEAN nations
+
     yearwise_pop = {}
+
+    # creates a dictionary of total population for each year
     for line in csv_read:
-        if(line[0] in SAARC):
+        if(line[0] in saarc):
             if(line[2] in yearwise_pop):
                 yearwise_pop[line[2]] += float(line[3])
             else:
                 yearwise_pop[line[2]] = float(line[3])
     plt.figure(figsize=(14, 6))
-    plt.bar(yearwise_pop.keys(), yearwise_pop.values(), label="SAARC")
-    plt.xlabel('Year')
-    plt.ylabel('Total Population')
-    plt.title("SAARC - Total population vs Years")
+    plt.bar(yearwise_pop.keys(), yearwise_pop.values())
+    plt.xlabel("Year")
+    plt.ylabel("Total Population")
+    plt.title("SAARC -Yea vs Total population")
     plt.xticks(rotation=90)
     plt.show()
 
 
-def group_plot_asean(csv_read, ASEAN):
+def group_plot_asean(csv_read, asean):
+    # function to create group plot for ASEAN countries
+
     population = {}
-    years = ["2009", "2010", "2011", "2012", "2013", "2014"]
+    years = [
+            "2009",
+            "2010",
+            "2011",
+            "2012",
+            "2013",
+            "2014"
+             ]
     x = np.arange(len(years))
     bwidth = 0.1
     fig, ax = plt.subplots()
     rects = [x]
+
+    # loop through the year and creates a dictionary of countrywise population
     for i, year in enumerate(years):
         for line in csv_read:
-            if(line[2] == year and line[0] in ASEAN):
+            if(line[2] == year and line[0] in asean):
                 if(line[0] in population):
                     population[line[0]].append(float(line[3]))
                 else:
-                    population[line[0]]=[float(line[3])]
+                    population[line[0]] = [float(line[3])]
+
+    # creates a bar plot for each country
     for i, j in enumerate(population.keys()):
         rects.append(ax.bar(x+bwidth*i, population[j], width=bwidth, label=j))
-    ax.set_xticks(x)
+
+    ax.set_xticks(x+bwidth*i/2)
     ax.set_title("Population vs Year for each ASEAN nation")
-    ax.set_xlabel("Years")
+    ax.set_xlabel("Year")
     ax.set_ylabel("Population")
     ax.set_xticklabels(years)
     ax.legend()
@@ -80,31 +106,39 @@ def group_plot_asean(csv_read, ASEAN):
 
 
 if __name__ == "__main__":
-    SAARC = [
-             "Afghanistan",
-             "Bangladesh",
-             "Bhutan",
-             "India",
-             "Maldives",
-             "Nepal",
-             "Pakistan",
-             "Sri Lanka"
+
+    # list of SAARC nations
+    saarc = [
+            "Afghanistan",
+            "Bangladesh",
+            "Bhutan",
+            "India",
+            "Maldives",
+            "Nepal",
+            "Pakistan",
+            "Sri Lanka"
              ]
-    ASEAN = [
-             "Singapore",
-             "Brunei",
-             "Malaysia",
-             "Thailand",
-             "Cambodia",
-             "Indonesia",
-             "Laos",
-             "Myanmar",
-             "Philippines",
-             "Vietnam"
-            ]
+
+    # list of ASEAN nations
+    asean = [
+            "Singapore",
+            "Brunei",
+            "Malaysia",
+            "Thailand",
+            "Cambodia",
+            "Indonesia",
+            "Laos",
+            "Myanmar",
+            "Philippines",
+            "Vietnam"
+             ]
+
+    # opening the csv file and writing to a list
     with open('/home/chris/Downloads/popest.csv', 'r') as newfile:
         csv_read = list(csv.reader(newfile, delimiter=','))
+
+        # making function calls to plot the required graphs
         india_plot(csv_read)
-        asean_plot(csv_read, ASEAN)
-        saarc_plot(csv_read, SAARC)
-        group_plot_asean(csv_read, ASEAN)
+        asean_plot(csv_read, asean)
+        saarc_plot(csv_read, saarc)
+        group_plot_asean(csv_read, asean)
